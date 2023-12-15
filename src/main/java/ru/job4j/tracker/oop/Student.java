d3973a3467290ef924f4445449335956677d9686
venkos <vvvenkos@gmail.com>
2023-12-15 14:16:23 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4j/tracker/oop/Student.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.oop;
+
+public class Student {
+
+    public void music() {
+        System.out.println("Tra ta ta");
+    }
+
+    public void sing() {
+        System.out.println("I believe I an fly");
+    }
+    public static void main(String[] args) {
+        Student petya = new Student();
+        for (int i = 0; i < 3; i++) {
+            petya.music();
+        }
+        for (int i = 0; i < 3; i++) {
+            petya.sing();
+        }
+
+    }
+}
